e5640442cfede914d94544c09a6ca9dc7d370116
SheepTester <ussa.say@gmail.com>
2020-06-17 21:15:43 -0700

    Centralize a bunch of maps in a goroutine to avoid race conditions Concurrency is rather different in Go! Also, maps are apparently by reference or whatever

--- main.go
+++ main.go
@@ -22,14 +22,37 @@ import (
 var token string
 
 var order int = 3
-// This maps a sequence of strings (joined by a space) to a map of next words and frequencies
-var markov map[string]map[string]int
+var markovAdderChannel chan<- map[string]map[string]int
 
-// Maps channel ID to last words in that channel
-var channelLastWords map[string][]string
+func markovAdder(markov map[string]map[string]int, channel <-chan map[string]map[string]int, saveChannel chan<- map[string]map[string]int) {
+	for miniMarkov := range channel {
+		// Merge miniMarkov into big markov
+		for context, newFrequencies := range miniMarkov {
+			frequencies, ok := markov[context]
+			if !ok {
+				frequencies = make(map[string]int)
+				markov[context] = frequencies
+			}
+			for possibility, frequency := range newFrequencies {
+				frequencies[possibility] += frequency
+			}
+		}
+		saveChannel <- markov
+	}
+}
 
-var path string = "./data/frequencies.json"
-var saveChannel chan map[string]map[string]int
+var requestLastWords chan<- string
+var getLastWords <-chan []string
+var saveLastWords chan<- []string
+
+func lastWordsTracker(requestChan <-chan string, getChan chan<- []string, saveChan <-chan []string)  {
+	// Maps channel ID to last words in that channel
+	channelLastWords := make(map[string][]string)
+	for channelID := range requestChan {
+		getChan <- channelLastWords[channelID]
+		channelLastWords[channelID] = <- saveChan
+	}
+}
 
 var filterChars = regexp.MustCompile(`[^a-z0-9\s?]`)
 var getWord = regexp.MustCompile(`\w+|\?`)
@@ -51,10 +74,7 @@ func hasWord(words []string, target string) bool {
 	return false
 }
 
-func generate(start string) string {
-	//
-	return ""
-}
+var path string = "./data/frequencies.json"
 
 // Thanks https://www.youtube.com/watch?v=LvgVSSpwND8 :D
 func markovSaver(channel <-chan map[string]map[string]int) {
@@ -83,8 +103,8 @@ func init() {
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	channelLastWords = make(map[string][]string, 10)
-
+	// This maps a sequence of strings (joined by a space) to a map of next words and frequencies
+	markov := make(map[string]map[string]int)
 	data, err := ioutil.ReadFile(path)
   if err == nil {
 		err = json.Unmarshal(data, &markov)
@@ -92,13 +112,27 @@ func main() {
 			fmt.Println("Problem parsing frequencies JSON,", err)
 			return
 		}
-  } else {
-		markov = make(map[string]map[string]int)
-	}
+  }
 
-	saveChannel = make(chan map[string]map[string]int)
+	requestLastWordsChan := make(chan string, 100)
+	defer close(requestLastWordsChan)
+	getLastWordsChan := make(chan []string, 100)
+	defer close(getLastWordsChan)
+	saveLastWordsChan := make(chan []string, 100)
+	defer close(saveLastWordsChan)
+	go lastWordsTracker(requestLastWordsChan, getLastWordsChan, saveLastWordsChan)
+	requestLastWords = requestLastWordsChan
+	getLastWords = getLastWordsChan
+	saveLastWords = saveLastWordsChan
+
+	saveChannel := make(chan map[string]map[string]int, 100)
+	defer close(saveChannel)
 	go markovSaver(saveChannel)
-	defer close(saveChannel) // Probably not necessary but defer is sooo cool lol
+
+	markovAdderBiChannel := make(chan map[string]map[string]int, 100)
+	defer close(markovAdderBiChannel)
+	go markovAdder(markov, markovAdderBiChannel, saveChannel)
+	markovAdderChannel = markovAdderBiChannel
 
 	dg, err := discordgo.New("Bot " + token)
 	if err != nil {
@@ -128,29 +162,31 @@ func messageCreate(session *discordgo.Session, msg *discordgo.MessageCreate) {
 	}
 
 	words := simplify(msg.Content)
-
 	// Message cannot be empty
-	if len(words) > 0 {
-		lastWords := channelLastWords[msg.ChannelID]
-		sequence := append(append(lastWords, "/"), words...)
-		for i, word := range sequence {
-			if i >= order {
-				context := strings.Join(sequence[i - order : i], " ")
-				frequencies, ok := markov[context]
-				if !ok {
-					frequencies = make(map[string]int)
-					markov[context] = frequencies
-				}
-				frequencies[word]++
-			}
-		}
-		if len(sequence) < order {
-			channelLastWords[msg.ChannelID] = sequence
-		} else {
-			channelLastWords[msg.ChannelID] = sequence[len(sequence) - order :]
-		}
-		saveChannel <- markov
+	if len(words) == 0 {
+		return
 	}
+
+	lastWords := <- getLastWords
+	sequence := append(append(lastWords, "/"), words...)
+	miniMarkov := make(map[string]map[string]int)
+	for i, word := range sequence {
+		if i >= order {
+			context := strings.Join(sequence[i - order : i], " ")
+			frequencies, ok := miniMarkov[context]
+			if !ok {
+				frequencies = make(map[string]int)
+				miniMarkov[context] = frequencies
+			}
+			frequencies[word]++
+		}
+	}
+	if len(sequence) < order {
+		saveLastWords <- sequence
+	} else {
+		saveLastWords <- sequence[len(sequence) - order :]
+	}
+	markovAdderChannel <- miniMarkov
 
 	// if rand.Intn(2) == 0 {
 	// 	session.ChannelMessageSend(msg.ChannelID, generate())
